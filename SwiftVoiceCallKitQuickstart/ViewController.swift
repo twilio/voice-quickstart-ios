d96e4ef0fc791b640fc8b8a4d2c2d6803c2aab92
bobiechen-twilio <bchen@twilio.com>
2018-12-26 16:21:20 -0800

    Add userInitiatedDisconnect flag to properly end CallKit calls (#200)

--- SwiftVoiceCallKitQuickstart/ViewController.swift
+++ SwiftVoiceCallKitQuickstart/ViewController.swift
@@ -26,19 +26,20 @@ class ViewController: UIViewController, PKPushRegistryDelegate, TVONotificationD
     @IBOutlet weak var muteSwitch: UISwitch!
     @IBOutlet weak var speakerSwitch: UISwitch!
     
-    var deviceTokenString:String?
+    var deviceTokenString: String?
 
-    var voipRegistry:PKPushRegistry
+    var voipRegistry: PKPushRegistry
 
     var isSpinning: Bool
     var incomingAlertController: UIAlertController?
 
-    var callInvite:TVOCallInvite?
-    var call:TVOCall?
+    var callInvite: TVOCallInvite?
+    var call: TVOCall?
     var callKitCompletionCallback: ((Bool)->Swift.Void?)? = nil
 
-    let callKitProvider:CXProvider
-    let callKitCallController:CXCallController
+    let callKitProvider: CXProvider
+    let callKitCallController: CXCallController
+    var userInitiatedDisconnect: Bool = false
 
     required init?(coder aDecoder: NSCoder) {
         isSpinning = false
@@ -102,6 +103,7 @@ class ViewController: UIViewController, PKPushRegistryDelegate, TVONotificationD
 
     @IBAction func placeCall(_ sender: UIButton) {
         if (self.call != nil && self.call?.state == .connected) {
+            self.userInitiatedDisconnect = true
             performEndCallAction(uuid: self.call!.uuid)
             self.toggleUIState(isEnabled: false, showCallControl: false)
         } else {
@@ -279,12 +281,23 @@ class ViewController: UIViewController, PKPushRegistryDelegate, TVONotificationD
             NSLog("Call disconnected")
         }
 
+        if !self.userInitiatedDisconnect {
+            var reason = CXCallEndedReason.remoteEnded
+
+            if error != nil {
+                reason = .failed
+            }
+
+            self.callKitProvider.reportCall(with: call.uuid, endedAt: Date(), reason: reason)
+        }
+
         callDisconnected()
     }
     
     func callDisconnected() {
         self.call = nil
         self.callKitCompletionCallback = nil
+        self.userInitiatedDisconnect = false
         
         stopSpin()
         toggleUIState(isEnabled: true, showCallControl: false)
