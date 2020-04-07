5d5205db05a0b5b3afb0b5aeb0eb3f00b0259327
Bobie Chen <bchen@twilio.com>
2020-04-07 15:29:11 -0700

    Update AudioDeviceExample project

---
+++ AudioDeviceExample/AudioDeviceExample/AudioDevices/ExampleAVAudioEngineDevice.h
@@ -0,0 +1,17 @@
+//
+//  ExampleAVAudioEngineDevice.h
+//  AudioDeviceExample
+//
+//  Copyright © 2018-2020 Twilio Inc. All rights reserved.
+//
+
+@import TwilioVoice;
+
+NS_CLASS_AVAILABLE(NA, 11_0)
+@interface ExampleAVAudioEngineDevice : NSObject <TVOAudioDevice>
+
+@property (nonatomic, assign, getter=isEnabled) BOOL enabled;
+
+- (void)playMusic;
+
+@end
--- AudioDeviceExample/AudioDeviceExample/ViewController.swift
+++ AudioDeviceExample/AudioDeviceExample/ViewController.swift
@@ -5,15 +5,202 @@
 //  Copyright © 2020 Twilio, Inc. All rights reserved.
 //
 
+import CallKit
 import UIKit
 
+let twimlParamTo = "to"
+
 class ViewController: UIViewController {
+    
+    var accessToken: String? = <#Replace with Access Token string#>
+    var activeCall: TVOCall?
+    var audioDevice: ExampleAVAudioEngineDevice = ExampleAVAudioEngineDevice()
+    
+    var callKitProvider: CXProvider
+    var callKitCallController: CXCallController
+    var userInitiatedDisconnect: Bool = false
+    var callKitCompletionCallback: ((Bool) -> Void)? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
 
+        setUpCallKit()
+    }
+    
+    deinit {
+        // CallKit has an odd API contract where the developer must call invalidate or the CXProvider is leaked.
+        callKitProvider.invalidate()
+    }
+    
+    func setUpCallKit() {
+        let configuration = CXProviderConfiguration(localizedName: "Quickstart")
+         configuration.maximumCallGroups = 1
+         configuration.maximumCallsPerCallGroup = 1
+
+         callKitProvider = CXProvider(configuration: configuration)
+         callKitCallController = CXCallController()
+        
+        callKitProvider.setDelegate(self, queue: nil)
+    }
+
+}
+
+// MARK: - CXProviderDelegate
+
+extension ViewController: CXProviderDelegate {
+    func providerDidReset(_ provider: CXProvider) {
+        NSLog("providerDidReset:")
+        audioDevice.isEnabled = true
+    }
+
+    func providerDidBegin(_ provider: CXProvider) {
+        NSLog("providerDidBegin")
+    }
+
+    func provider(_ provider: CXProvider, didActivate audioSession: AVAudioSession) {
+        NSLog("provider:didActivateAudioSession:")
+        audioDevice.isEnabled = true
+    }
+
+    func provider(_ provider: CXProvider, didDeactivate audioSession: AVAudioSession) {
+        NSLog("provider:didDeactivateAudioSession:")
+    }
+
+    func provider(_ provider: CXProvider, timedOutPerforming action: CXAction) {
+        NSLog("provider:timedOutPerformingAction:")
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXStartCallAction) {
+        NSLog("provider:performStartCallAction:")
+
+        audioDevice.isEnabled = false
+        
+        provider.reportOutgoingCall(with: action.callUUID, startedConnectingAt: Date())
+        
+        performVoiceCall(uuid: action.callUUID, client: "") { success in
+            if success {
+                provider.reportOutgoingCall(with: action.callUUID, connectedAt: Date())
+                action.fulfill()
+            } else {
+                action.fail()
+            }
+        }
+    }
+
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        NSLog("provider:performEndCallAction:")
+        
+        if let call = self.activeCall {
+            call.disconnect()
+        }
+
+        action.fulfill()
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXSetHeldCallAction) {
+        NSLog("provider:performSetHeldAction:")
+        
+        if let call = self.activeCall {
+            call.isOnHold = action.isOnHold
+            action.fulfill()
+        } else {
+            action.fail()
+        }
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXSetMutedCallAction) {
+        NSLog("provider:performSetMutedAction:")
+
+        if let call = self.activeCall {
+            call.isMuted = action.isMuted
+            action.fulfill()
+        } else {
+            action.fail()
+        }
+    }
+
+    
+    // MARK: Call Kit Actions
+    func performStartCallAction(uuid: UUID, handle: String) {
+        let callHandle = CXHandle(type: .generic, value: handle)
+        let startCallAction = CXStartCallAction(call: uuid, handle: callHandle)
+        let transaction = CXTransaction(action: startCallAction)
+
+        callKitCallController.request(transaction) { error in
+            if let error = error {
+                NSLog("StartCallAction transaction request failed: \(error.localizedDescription)")
+                return
+            }
+
+            NSLog("StartCallAction transaction request successful")
+
+            let callUpdate = CXCallUpdate()
+            
+            callUpdate.remoteHandle = callHandle
+            callUpdate.supportsDTMF = true
+            callUpdate.supportsHolding = true
+            callUpdate.supportsGrouping = false
+            callUpdate.supportsUngrouping = false
+            callUpdate.hasVideo = false
+
+            self.callKitProvider.reportCall(with: uuid, updated: callUpdate)
+        }
+    }
+
+    func reportIncomingCall(from: String, uuid: UUID) {
+        let callHandle = CXHandle(type: .generic, value: from)
+
+        let callUpdate = CXCallUpdate()
+        
+        callUpdate.remoteHandle = callHandle
+        callUpdate.supportsDTMF = true
+        callUpdate.supportsHolding = true
+        callUpdate.supportsGrouping = false
+        callUpdate.supportsUngrouping = false
+        callUpdate.hasVideo = false
+
+        callKitProvider.reportNewIncomingCall(with: uuid, update: callUpdate) { error in
+            if let error = error {
+                NSLog("Failed to report incoming call successfully: \(error.localizedDescription).")
+            } else {
+                NSLog("Incoming call successfully reported.")
+            }
+        }
+    }
+
+    func performEndCallAction(uuid: UUID) {
+
+        let endCallAction = CXEndCallAction(call: uuid)
+        let transaction = CXTransaction(action: endCallAction)
+
+        callKitCallController.request(transaction) { error in
+            if let error = error {
+                NSLog("EndCallAction transaction request failed: \(error.localizedDescription).")
+            } else {
+                NSLog("EndCallAction transaction request successful")
+            }
+        }
+    }
+    
+    func performVoiceCall(uuid: UUID, client: String?, completionHandler: @escaping (Bool) -> Void) {
+        guard let token = accessToken, token.count > 0 else {
+            completionHandler(false)
+            return
+        }
+        
+        let connectOptions = TVOConnectOptions(accessToken: token) { builder in
+            builder.params = [twimlParamTo: self.outgoingValue.text ?? ""]
+            builder.uuid = uuid
+        }
+        
+        let call = TwilioVoice.connect(with: connectOptions, delegate: self)
+        activeCall = call
+        callKitCompletionCallback = completionHandler
+    }
 }
 
