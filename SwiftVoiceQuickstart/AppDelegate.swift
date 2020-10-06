bfc038e4bac7b9d331e65520045ac44db7ed6db4
ptank <ptank@twilio.com>
2020-10-06 12:00:57 -0700

    Added documentation for delayed push notification

--- SwiftVoiceQuickstart/AppDelegate.swift
+++ SwiftVoiceQuickstart/AppDelegate.swift
@@ -28,6 +28,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
         
         let viewController = UIApplication.shared.windows.first?.rootViewController as? ViewController
         self.pushKitEventDelegate = viewController
+        /*
+         * Your app must initialize PKPushRegistry with PushKit push type VoIP at the launch time. As mentioned in the
+         * [PushKit guidelines](https://developer.apple.com/documentation/pushkit/supporting_pushkit_notifications_in_your_app),
+         * the system can't deliver push notifications to your app until you create a PKPushRegistry object for
+         * VoIP push type and set the delegate. If your app delays the initialization of PKPushRegistry, your app may receive outdated
+         * PushKit push notifications, and if your app decides not to report the received outdated push notifications to CallKit, iOS may
+         * terminate your app.
+         */
         initializePushKit()
 
         return true
