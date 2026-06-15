815d9579dfa9c563927d61789c36e2a144ebe542
Bobie Chen <bchen@twilio.com>
2026-06-15 16:24:23 -0700

    go UIScene

---
+++ LiveCommunicationKitExample/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Twilio Voice Quickstart - LiveCommunicationKit
+//
+//  Copyright © Twilio, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
